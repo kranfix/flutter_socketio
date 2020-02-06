d7484cee823e510ffa668c4c0529e35be545ce1c
Frank Moreno <frankmoreno1993@gmail.com>
2020-02-06 11:29:52 -0500

    creating file fior DashboardScreen: Missing implementation

---
+++ lib/src/ui/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Missing Implementation'),
+      ),
+    );
+  }
+}
