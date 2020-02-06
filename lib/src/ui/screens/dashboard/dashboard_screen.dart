65a4d56273c2e533b5288ed3aa26c2aabfda07a2
Frank Moreno <frankmoreno1993@gmail.com>
2020-02-06 13:09:50 -0500

    Creating ListView for users

--- lib/src/ui/screens/dashboard/dashboard_screen.dart
+++ lib/src/ui/screens/dashboard/dashboard_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_socketio/src/colors/colors.dart';
+import 'package:flutter_socketio/src/models/models.dart';
 import 'package:flutter_socketio/src/providers/providers.dart';
+import 'package:flutter_socketio/src/services/services.dart';
 import 'package:flutter_socketio/src/ui/widgets/my_app_bar.dart';
 import 'package:flutter_socketio/src/ui/widgets/socketio_logo.dart';
 
@@ -26,11 +30,69 @@ class DashboardScreen extends StatelessWidget {
   }
 }
 
-class _DashboardBody extends StatelessWidget {
+class _DashboardBody extends StatefulWidget {
+  @override
+  __DashboardBodyState createState() => __DashboardBodyState();
+}
+
+class __DashboardBodyState extends State<_DashboardBody> {
+  List<User> users;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scheduleMicrotask(initFuture);
+  }
+
+  Future<void> initFuture() async {
+    users ??= [];
+    try {
+      final userListData = await Services.user.fetchMany();
+      users.addAll(userListData.users);
+    } catch (e) {}
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Missing implementation'),
+    if (users == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (users.isEmpty) {
+      return Center(
+        child: Text("There's no available users"),
+      );
+    }
+
+    return ListView.builder(
+      padding: const EdgeInsets.all(20),
+      itemCount: users.length,
+      itemBuilder: (_, index) {
+        final u = users[index];
+        return ListTile(
+          leading: Container(
+            margin: const EdgeInsets.only(right: 16),
+            width: 55,
+            height: 55,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              border: Border.all(color: AppColors.grey.light1, width: 1),
+              image: DecorationImage(
+                image: NetworkImage(u.avatar),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          title: Text('${u.firstName} ${u.lastName}'),
+          subtitle: Text('${u.email}'),
+          contentPadding: const EdgeInsets.symmetric(vertical: 4),
+        );
+      },
     );
   }
 }
