6eccca4ee97b31c178f162aff410b6bb05b3a711
Frank Moreno <frankmoreno1993@gmail.com>
2020-02-25 14:38:37 -0500

    IoBloc handling connections

--- lib/src/providers/io_bloc.dart
+++ lib/src/providers/io_bloc.dart
@@ -1,38 +1,67 @@
 part of 'providers.dart';
 
 class IoBloc extends ChangeNotifier {
+  static const topic = "news";
+
   IoBloc({String server}) {
     init(server);
   }
 
+  List<Message> _messages;
+  List<Message> get messages => _messages;
+
   Future<void> init(String server) async {
     SocketIOManager manager = SocketIOManager();
-    SocketOptions options = SocketOptions(server);
-    final socket = await manager.createInstance(options);
-    socket.onConnect((data) {
-      print('connected: $data');
-      socket.emit('news', ['hola', 'chau']);
+    SocketOptions options = SocketOptions(
+      server,
+      enableLogging: false,
+    );
+    _socket = await manager.createInstance(options);
+    _socket.onConnect((data) {
+      print('Connected wit data: $data');
+      sendMessage(message: "Hello, Flutter!");
     });
-    socket.on("news", (data) {
-      //sample event
-      print("news");
-      print(data);
-    });
-    socket.connect();
-    socket.on('new message', (data) => print(data));
-    socket.on('disconnect', (_) => print('disconnect'));
-    socket.on('fromServer', (_) => print(_));
+    _socket.connect();
+    _socket.on(topic, onNewMessage);
+    _socket.on('disconnect', (_) => print('disconnect'));
+    _socket.on('fromServer', (_) => print(_));
   }
 
-  SocketIO socket;
+  SocketIO _socket;
 
-  void sendMessage({User user, dynamic message}) {
+  void sendMessage({dynamic message}) {
     final data = <String, dynamic>{
       'username': AuthBloc.username,
-      'user': user.$data,
+      Message.secretTag: AuthBloc.user.$data,
       'message': message,
     };
-    print(data);
-    //socket.emit('new message', [data]);
+    _socket.emit(topic, [data]);
+    _addMessage(Message.fromMap(data));
+  }
+
+  void onNewMessage(dynamic data) {
+    print('onNewMessage: $data');
+
+    final m = Message.fromMap(data);
+    print(m.user.$data);
+    return _addMessage(Message.fromMap(data));
+  }
+
+  void _addMessage(Message message) {
+    const maxMessages = 50;
+    if (_messages == null) {
+      _messages = [];
+    } else if (_messages.length == maxMessages) {
+      _messages.removeAt(0);
+    }
+
+    _messages.add(message);
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _messages = null;
+    super.dispose();
   }
 }
